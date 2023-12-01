4831b89200438afc00ba8bffba1dd078223ddda7
obenkploes <jokokristianto23@guru.smk.belajar.id>
2023-12-02 06:25:14 +0700

    update jurusan

--- src/app/jurusan/page.jsx
+++ src/app/jurusan/page.jsx
@@ -3,124 +3,155 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { MdCancel, MdDelete, MdEdit, MdSave } from "react-icons/md"
+import Notifikasi from "@/components/Notifikasi"
 
 const { Card, Row, Col, Form, Table, Button } = require("react-bootstrap")
 
-const Jurusan =()=>{
+const Jurusan = () => {
     // variable
-    const [listJurusan,setListJurusan]=useState([])
-    const [jurusan,setJurusan]=useState({
-        id:0,
-        kode_jurusan:'',
-        deskripsi:''
+    const [listJurusan, setListJurusan] = useState([])
+    const [jurusan, setJurusan] = useState({
+        id: 0,
+        kode_jurusan: '',
+        deskripsi: ''
+    })
+    const [notif, setNotif] = useState({
+        show: false,
+        title: '',
+        message: ''
     })
 
-    const handleInput=e=>{
-        const {name,value}= e.target
-        setJurusan(prev=>({
+    // handle notifikasi
+    const handleNotifikasi = (show, title, message) => {
+        setNotif({
+            show: show,
+            title: title,
+            message: message
+        })
+    }
+    const handleInput = e => {
+        const { name, value } = e.target
+        setJurusan(prev => ({
             ...prev,
-            [name]:value
+            [name]: value
         }))
     }
-    const handleCancel = ()=>{
+    const handleCancel = () => {
         setJurusan({
-            id:0,
-            kode_jurusan:'',
-            deskripsi:'' 
+            id: 0,
+            kode_jurusan: '',
+            deskripsi: ''
         })
         document.getElementById('kode_jurusan').focus()
     }
-    const handleEdit=(dt)=>{
+    const handleEdit = (dt) => {
         setJurusan(dt)
         document.getElementById('kode_jurusan').focus()
     }
-    const handleSubmit=async(e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        let mt = jurusan.id >0 ?'put':'post'
+        handleNotifikasi(false,'','')
+        let mt = jurusan.id > 0 ? 'put' : 'post'
         await axios({
-            url:'api/jurusan',
-            method:mt,
-            data:jurusan
+            url: 'api/jurusan',
+            method: mt,
+            data: jurusan
         })
-        .then(res=>console.log(res.data))
-        .catch(err=>console.log(err))
+            .then(res => {
+                let list = listJurusan
+                if (jurusan.id>0) {
+                    let index = list.findIndex(el=>el.id==jurusan.id)
+                    list[index]=res.data.jurusan
+                    setListJurusan(list)
+                    handleNotifikasi(true,'Informasi','Data berhasil diperbaharui')
+                }else{
+                    list.push(res.data.jurusan)
+                    setListJurusan(list)
+                    handleNotifikasi(true,'Informasi','Data berhasil ditambahkan')
+                }
+                handleCancel()
+            })
+            .catch(err => console.log(err))
     }
-    useEffect(()=>{
-        const loadData=async()=>{
+    useEffect(() => {
+        const loadData = async () => {
             await axios({
-                url:'api/jurusan',method:'get'
+                url: 'api/jurusan', method: 'get'
             })
-            .then(res=>{
-                setListJurusan(res.data.jurusan)
-            })
-            .catch(err=>{
-                console.log(err);
-            })
+                .then(res => {
+                    setListJurusan(res.data.jurusan)
+                })
+                .catch(err => {
+                    console.log(err);
+                })
         }
         loadData()
-    },[])
-    return(
-        <Row >
-            <Col lg={3} >
-                <Card>
-                    <Card.Header className="text-bg-primary">
-                        <Card.Title>Form jurusan</Card.Title>
-                    </Card.Header>
-                    <Card.Body>
-                        <Form onSubmit={handleSubmit}>
-                            <Form.Group controlId="kode_jurusan" className="mb-3">
-                                <Form.Label>Kode Jurusan</Form.Label>
-                                <Form.Control type="text" name="kode_jurusan" placeholder="RPL" autoFocus onChange={e=>handleInput(e)} value={jurusan.kode_jurusan} required/>
-                            </Form.Group>
-                            <Form.Group controlId="deskripsi" className="mb-3">
-                                <Form.Label>Deskripsi</Form.Label>
-                                <Form.Control type="text" name="deskripsi" placeholder="Rekayasa Perangkat Lunak"  onChange={e=>handleInput(e)} value={jurusan.deskripsi} required/>
-                            </Form.Group>
-                            <Form.Group className="mb-3">
-                                <Button className="btn-primary  form-control" type="submit">
-                                    <MdSave className="mb-1" /> Simpan
-                                </Button>
-                                <Button className="btn-success  form-control mt-1" onClick={()=>handleCancel()}>
-                                    <MdCancel className="mb-1" /> Batal
-                                </Button>
-                            </Form.Group>
-                        </Form>
-                    </Card.Body>
-                </Card>
-            </Col>
-            <Col className="mt-md-2">
-                <Card>
-                    <Card.Header className="text-bg-primary">
-                        <Card.Title>Data jurusan</Card.Title>
-                    </Card.Header>
-                    <Card.Body>
-                        <Table className="table-sm table-bordered ">
-                            <thead>
-                                <tr>
-                                    <th>No</th>
-                                    <th>Kode jurusan</th>
-                                    <th>Deskripsi</th>
-                                    <th>#</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {listJurusan.length>0 && listJurusan.map((el,index)=>(
-                                    <tr key={index} className="align-middle">
-                                        <td>{index + 1}</td>
-                                        <td>{el.kode_jurusan}</td>
-                                        <td>{el.deskripsi}</td>
-                                        <td >
-                                            <Button className="btn-sm btn-success" onClick={()=>handleEdit(el)}><MdEdit /></Button>
-                                            <Button className="btn-sm btn-danger ms-1"><MdDelete /></Button>
-                                        </td>
+    }, [])
+    return (
+        <>
+            <Row >
+                <Col lg={3} >
+                    <Card>
+                        <Card.Header className="text-bg-primary">
+                            <Card.Title>Form jurusan</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group controlId="kode_jurusan" className="mb-3">
+                                    <Form.Label>Kode Jurusan</Form.Label>
+                                    <Form.Control type="text" name="kode_jurusan" placeholder="RPL" autoFocus onChange={e => handleInput(e)} value={jurusan.kode_jurusan} required />
+                                </Form.Group>
+                                <Form.Group controlId="deskripsi" className="mb-3">
+                                    <Form.Label>Deskripsi</Form.Label>
+                                    <Form.Control type="text" name="deskripsi" placeholder="Rekayasa Perangkat Lunak" onChange={e => handleInput(e)} value={jurusan.deskripsi} required />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Button className="btn-primary  form-control" type="submit">
+                                        <MdSave className="mb-1" /> Simpan
+                                    </Button>
+                                    <Button className="btn-success  form-control mt-1" onClick={() => handleCancel()}>
+                                        <MdCancel className="mb-1" /> Batal
+                                    </Button>
+                                </Form.Group>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col className="mt-md-2">
+                    <Card>
+                        <Card.Header className="text-bg-primary">
+                            <Card.Title>Data jurusan</Card.Title>
+                        </Card.Header>
+                        <Card.Body>
+                            <Table className="table-sm table-bordered ">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Kode jurusan</th>
+                                        <th>Deskripsi</th>
+                                        <th>#</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </Table>
-                    </Card.Body>
-                </Card>
-            </Col>
-        </Row>
+                                </thead>
+                                <tbody>
+                                    {listJurusan.length > 0 && listJurusan.map((el, index) => (
+                                        <tr key={index} className="align-middle">
+                                            <td>{index + 1}</td>
+                                            <td>{el.kode_jurusan}</td>
+                                            <td>{el.deskripsi}</td>
+                                            <td >
+                                                <Button className="btn-sm btn-success" onClick={() => handleEdit(el)}><MdEdit /></Button>
+                                                <Button className="btn-sm btn-danger ms-1"><MdDelete /></Button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+            <Notifikasi showToast={notif.show} close={()=>handleNotifikasi(false,'','')} title={notif.title} message={notif.message}/>
+        </>
     )
 }
 
--- src/components/Notifikasi.jsx
+++ src/components/Notifikasi.jsx
@@ -4,7 +4,7 @@ import { Toast, ToastContainer } from "react-bootstrap"
 
 export default function Notifikasi({ showToast, title, message, close }) {
     return (
-        <ToastContainer position="bottom-end" className="me-2 mb-2">
+        <ToastContainer position="top-end" className="me-2 mb-2">
             <Toast show={showToast} onClose={() => close()} delay={5000} autohide>
                 <Toast.Header>
                     <strong className="me-auto">{title}</strong>
